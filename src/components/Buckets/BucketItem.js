960cce92e019896c01314a9fe5e54edfb4150915
ankit <ankit.sari@gmail.com>
2018-04-09 10:28:42 -0700

    initial commit

---
+++ src/components/Buckets/BucketItem.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Draggable} from 'react-beautiful-dnd';
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  background: isDragging ? 'lightgreen' : '',
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const BucketItem = ({bucketItem: {ItemTitle, Description, ItemId}, index}) => {
+    return (
+      <Draggable key={ItemId} draggableId={ItemId} index={index}>
+        {(provided2, snapshot2) => (
+          <div>
+            <div className="card card-block item"
+                 ref={provided2.innerRef}
+                 {...provided2.draggableProps}
+                 {...provided2.dragHandleProps}
+                 style={getItemStyle(
+                   snapshot2.isDragging,
+                   provided2.draggableProps.style
+                 )}
+            >
+              <p><b>{ItemTitle}</b></p>
+              <p>{Description}</p>
+            </div>
+            {provided2.placeholder}
+          </div>
+        )}
+      </Draggable>
+    )
+}
+
+BucketItem.propTypes = {
+  bucketItem: PropTypes.shape({
+    ItemTitle: PropTypes.string,
+    Description: PropTypes.string,
+    ItemId: PropTypes.number,
+  }),
+  index: PropTypes.number,
+}
+
+BucketItem.defaultProps = {
+  bucketItem: {}
+}
+
+export default BucketItem
---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+const getURL = url => `http://roadmapservice.azurewebsites.net/api/${url}`
+
+
+const utils = {
+  getURL,
+}
+
+export default utils
