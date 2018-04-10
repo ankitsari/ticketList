89d054b13d608a7b634f655b03b6cdc19422731d
ankit <ankit.sari@gmail.com>
2018-04-10 07:44:27 -0700

    set url based one environment

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
-const getURL = url => `http://roadmapservice.azurewebsites.net/api/${url}`
+const getURL = url => process.env.environment === 'development' ? `http://roadmapservice.azurewebsites.net/api/${url}` :
+  `https://roadmapservice.azurewebsites.net/api/${url}`
 
 
 const utils = {
